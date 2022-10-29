197c36ba685caed7f99e6fc14ff67c93383c9def
muntaaha <muntaaha.rahman3001@gmail.com>
2022-10-29 18:51:41 +0600

    UserWIshlistModel prepared

---
+++ backend/models/UserWishlistModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const userWishlistSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50,
+    },
+    wishlistedMovies = Array,
+});
+
+module.exports = mongoose.model("user_wishlist", userWishlistSchema); 
